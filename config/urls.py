45b458f5740e73beeefa86a66b41dae7cc002fce
ssofpv <s_makarycheva.18@mail.ru>
2026-09-14 14:56:27 +0300

    Revert "feat: добавить веб-интерфейс филиалов и экземпляров"

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,6 @@ from django.urls import include, path
 from .views import health
 
 urlpatterns = [
-    path("", include("inventory.web_urls")),
     path("", include("catalog.web_urls")),
     path("admin/", admin.site.urls),
     path("health/", health, name="health"),
